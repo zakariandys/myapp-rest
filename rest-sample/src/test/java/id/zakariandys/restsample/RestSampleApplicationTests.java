0ae08b7b04365df875ca6dab4a341b783da6cd26
zakaria <zakaria.saputro@dana.id>
2023-08-21 09:00:27 +0700

    initital project

---
+++ rest-sample/src/test/java/id/zakariandys/restsample/RestSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.zakariandys.restsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
